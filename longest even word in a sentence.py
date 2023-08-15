88936ae8919bf02cac9f16a8bc20f10b0a548212
Amy Sun <111322085+sun-amy@users.noreply.github.com>
2023-08-15 15:17:45 +0100

    Create longest even word in a sentence.py

---
+++ longest even word in a sentence.py
@@ -0,0 +1,16 @@
+"""
+My brother mentioned this problem he came across browsing the internet.
+Given a sentence, return the longest word in that sentence that has an even number of letters.
+"""
+
+def solution(input_string):
+    words_as_list = input_string.split(" ")
+    evens = list()
+    for word in words_as_list:
+        if len(word) % 2 == 0:
+            evens.append(word)
+    longest_even = evens[0]
+    for i in range(len(evens) - 1):
+        if len(evens[i + 1]) > len(evens[i]):
+            longest_even = evens[i + 1]
+    return longest_even
